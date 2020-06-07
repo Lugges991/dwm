da650e4025b4fe0b0f4114d86daa68d90571ed0b
Lugges991 <29121982+Lugges991@users.noreply.github.com>
2020-06-07 10:31:51 +0200

    adjusted font size for larger resolutions

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DeJa Vu Sans Mono Nerd Font:size=13" };
-static const char dmenufont[]       = "DeJa Vu Sans Mono Nerd Font:size=13";
+static const char *fonts[]          = { "DeJa Vu Sans Mono Nerd Font:size=11" };
+static const char dmenufont[]       = "DeJa Vu Sans Mono Nerd Font:size=12";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
